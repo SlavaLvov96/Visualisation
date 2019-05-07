7a7f6774bfc9decc317da182ffaed96f2c700a8e
Mironov Dmitry Ivanovich <dmit.mironoff2016@yandex.ru>
2019-05-07 18:48:32 +0300

    Алгоритмы сортировки: добавление кода для инициализации значений

--- Visualisation/Visualisation/FormSort.Designer.cs
+++ Visualisation/Visualisation/FormSort.Designer.cs
@@ -29,112 +29,61 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormSort));
-            this.comboBoxSort = new System.Windows.Forms.ComboBox();
-            this.comboBoxSearch = new System.Windows.Forms.ComboBox();
-            this.comboBoxGraph = new System.Windows.Forms.ComboBox();
             this.buttonStart = new System.Windows.Forms.Button();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.buttonPrev = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.buttonStartPause = new System.Windows.Forms.Button();
             this.buttonNext = new System.Windows.Forms.Button();
             this.textBoxInText = new System.Windows.Forms.TextBox();
             this.textBoxOutText = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.richTextBox = new System.Windows.Forms.RichTextBox();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.buttonBubble = new System.Windows.Forms.Button();
+            this.buttonQuick = new System.Windows.Forms.Button();
+            this.buttonInsert = new System.Windows.Forms.Button();
+            this.buttonSelection = new System.Windows.Forms.Button();
+            this.buttonHeapsort = new System.Windows.Forms.Button();
+            this.buttonShell = new System.Windows.Forms.Button();
+            this.buttonTimsort = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
             this.SuspendLayout();
-            // 
-            // comboBoxSort
-            // 
-            this.comboBoxSort.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.comboBoxSort.FormattingEnabled = true;
-            this.comboBoxSort.Items.AddRange(new object[] {
-            "Merge",
-            "Shell",
-            "Bubble",
-            "Bucket",
-            "Insertion"});
-            this.comboBoxSort.Location = new System.Drawing.Point(3, 3);
-            this.comboBoxSort.Name = "comboBoxSort";
-            this.comboBoxSort.Size = new System.Drawing.Size(121, 21);
-            this.comboBoxSort.TabIndex = 1;
-            this.comboBoxSort.Text = "Sorting";
-            // 
-            // comboBoxSearch
-            // 
-            this.comboBoxSearch.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.comboBoxSearch.FormattingEnabled = true;
-            this.comboBoxSearch.Items.AddRange(new object[] {
-            "Sequential",
-            "Interval",
-            "Linear",
-            "Binary",
-            "Jump"});
-            this.comboBoxSearch.Location = new System.Drawing.Point(3, 27);
-            this.comboBoxSearch.Name = "comboBoxSearch";
-            this.comboBoxSearch.Size = new System.Drawing.Size(121, 21);
-            this.comboBoxSearch.TabIndex = 2;
-            this.comboBoxSearch.Text = "Searching";
-            // 
-            // comboBoxGraph
-            // 
-            this.comboBoxGraph.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.comboBoxGraph.FormattingEnabled = true;
-            this.comboBoxGraph.Items.AddRange(new object[] {
-            "Minimax",
-            "Kernel",
-            "Kruskal\'s",
-            "Tree traversal"});
-            this.comboBoxGraph.Location = new System.Drawing.Point(3, 51);
-            this.comboBoxGraph.Name = "comboBoxGraph";
-            this.comboBoxGraph.Size = new System.Drawing.Size(121, 21);
-            this.comboBoxGraph.TabIndex = 3;
-            this.comboBoxGraph.Text = "Graphs";
             // 
             // buttonStart
             // 
             this.buttonStart.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.buttonStart.Location = new System.Drawing.Point(27, 415);
+            this.buttonStart.Location = new System.Drawing.Point(729, 338);
             this.buttonStart.Name = "buttonStart";
             this.buttonStart.Size = new System.Drawing.Size(75, 23);
             this.buttonStart.TabIndex = 4;
-            this.buttonStart.Text = "Start";
+            this.buttonStart.Text = "Initialization";
             this.buttonStart.UseVisualStyleBackColor = true;
-            // 
-            // dataGridView1
-            // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(130, 3);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(591, 435);
-            this.dataGridView1.TabIndex = 5;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
             // 
             // buttonPrev
             // 
             this.buttonPrev.Cursor = System.Windows.Forms.Cursors.Hand;
             this.buttonPrev.Image = ((System.Drawing.Image)(resources.GetObject("buttonPrev.Image")));
-            this.buttonPrev.Location = new System.Drawing.Point(729, 388);
+            this.buttonPrev.Location = new System.Drawing.Point(730, 444);
             this.buttonPrev.Name = "buttonPrev";
             this.buttonPrev.Size = new System.Drawing.Size(50, 50);
             this.buttonPrev.TabIndex = 6;
             this.buttonPrev.UseVisualStyleBackColor = true;
             // 
-            // button2
+            // buttonStartPause
             // 
-            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.button2.Image = ((System.Drawing.Image)(resources.GetObject("button2.Image")));
-            this.button2.Location = new System.Drawing.Point(785, 388);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(50, 50);
-            this.button2.TabIndex = 7;
-            this.button2.UseVisualStyleBackColor = true;
+            this.buttonStartPause.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonStartPause.Image = ((System.Drawing.Image)(resources.GetObject("buttonStartPause.Image")));
+            this.buttonStartPause.Location = new System.Drawing.Point(786, 444);
+            this.buttonStartPause.Name = "buttonStartPause";
+            this.buttonStartPause.Size = new System.Drawing.Size(50, 50);
+            this.buttonStartPause.TabIndex = 7;
+            this.buttonStartPause.UseVisualStyleBackColor = true;
             // 
             // buttonNext
             // 
             this.buttonNext.Cursor = System.Windows.Forms.Cursors.Hand;
             this.buttonNext.Image = ((System.Drawing.Image)(resources.GetObject("buttonNext.Image")));
-            this.buttonNext.Location = new System.Drawing.Point(841, 388);
+            this.buttonNext.Location = new System.Drawing.Point(842, 444);
             this.buttonNext.Name = "buttonNext";
             this.buttonNext.Size = new System.Drawing.Size(50, 50);
             this.buttonNext.TabIndex = 8;
@@ -146,11 +95,10 @@
             this.textBoxInText.Name = "textBoxInText";
             this.textBoxInText.Size = new System.Drawing.Size(255, 20);
             this.textBoxInText.TabIndex = 9;
-            this.textBoxInText.Text = "Введите значения...";
             // 
             // textBoxOutText
             // 
-            this.textBoxOutText.Location = new System.Drawing.Point(729, 362);
+            this.textBoxOutText.Location = new System.Drawing.Point(730, 418);
             this.textBoxOutText.Name = "textBoxOutText";
             this.textBoxOutText.Size = new System.Drawing.Size(255, 20);
             this.textBoxOutText.TabIndex = 10;
@@ -158,7 +106,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(727, 346);
+            this.label1.Location = new System.Drawing.Point(728, 402);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(40, 13);
             this.label1.TabIndex = 11;
@@ -181,46 +129,130 @@
             this.richTextBox.TabIndex = 13;
             this.richTextBox.Text = "";
             // 
+            // buttonBubble
+            // 
+            this.buttonBubble.Location = new System.Drawing.Point(0, 3);
+            this.buttonBubble.Name = "buttonBubble";
+            this.buttonBubble.Size = new System.Drawing.Size(124, 23);
+            this.buttonBubble.TabIndex = 14;
+            this.buttonBubble.Text = "Пузырьковая";
+            this.buttonBubble.UseVisualStyleBackColor = true;
+            this.buttonBubble.Click += new System.EventHandler(this.buttonBubble_Click);
+            // 
+            // buttonQuick
+            // 
+            this.buttonQuick.Location = new System.Drawing.Point(0, 32);
+            this.buttonQuick.Name = "buttonQuick";
+            this.buttonQuick.Size = new System.Drawing.Size(124, 23);
+            this.buttonQuick.TabIndex = 15;
+            this.buttonQuick.Text = "Быстрая";
+            this.buttonQuick.UseVisualStyleBackColor = true;
+            this.buttonQuick.Click += new System.EventHandler(this.buttonQuick_Click);
+            // 
+            // buttonInsert
+            // 
+            this.buttonInsert.Location = new System.Drawing.Point(0, 119);
+            this.buttonInsert.Name = "buttonInsert";
+            this.buttonInsert.Size = new System.Drawing.Size(124, 23);
+            this.buttonInsert.TabIndex = 16;
+            this.buttonInsert.Text = "Вставками";
+            this.buttonInsert.UseVisualStyleBackColor = true;
+            this.buttonInsert.Click += new System.EventHandler(this.buttonInsert_Click);
+            // 
+            // buttonSelection
+            // 
+            this.buttonSelection.Location = new System.Drawing.Point(0, 61);
+            this.buttonSelection.Name = "buttonSelection";
+            this.buttonSelection.Size = new System.Drawing.Size(124, 23);
+            this.buttonSelection.TabIndex = 17;
+            this.buttonSelection.Text = "Выбором";
+            this.buttonSelection.UseVisualStyleBackColor = true;
+            this.buttonSelection.Click += new System.EventHandler(this.buttonSelection_Click);
+            // 
+            // buttonHeapsort
+            // 
+            this.buttonHeapsort.Location = new System.Drawing.Point(0, 90);
+            this.buttonHeapsort.Name = "buttonHeapsort";
+            this.buttonHeapsort.Size = new System.Drawing.Size(124, 23);
+            this.buttonHeapsort.TabIndex = 18;
+            this.buttonHeapsort.Text = "Пирамидальная";
+            this.buttonHeapsort.UseVisualStyleBackColor = true;
+            this.buttonHeapsort.Click += new System.EventHandler(this.buttonHeapsort_Click);
+            // 
+            // buttonShell
+            // 
+            this.buttonShell.Location = new System.Drawing.Point(0, 148);
+            this.buttonShell.Name = "buttonShell";
+            this.buttonShell.Size = new System.Drawing.Size(124, 23);
+            this.buttonShell.TabIndex = 19;
+            this.buttonShell.Text = "Шелла";
+            this.buttonShell.UseVisualStyleBackColor = true;
+            this.buttonShell.Click += new System.EventHandler(this.buttonShell_Click);
+            // 
+            // buttonTimsort
+            // 
+            this.buttonTimsort.Location = new System.Drawing.Point(0, 177);
+            this.buttonTimsort.Name = "buttonTimsort";
+            this.buttonTimsort.Size = new System.Drawing.Size(124, 23);
+            this.buttonTimsort.TabIndex = 20;
+            this.buttonTimsort.Text = "TimSort";
+            this.buttonTimsort.UseVisualStyleBackColor = true;
+            this.buttonTimsort.Click += new System.EventHandler(this.buttonTimsort_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(130, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(593, 445);
+            this.panel1.TabIndex = 21;
+            // 
             // FormSort
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(994, 446);
+            this.ClientSize = new System.Drawing.Size(1011, 501);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.buttonTimsort);
+            this.Controls.Add(this.buttonShell);
+            this.Controls.Add(this.buttonHeapsort);
+            this.Controls.Add(this.buttonSelection);
+            this.Controls.Add(this.buttonInsert);
+            this.Controls.Add(this.buttonQuick);
+            this.Controls.Add(this.buttonBubble);
             this.Controls.Add(this.richTextBox);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBoxOutText);
             this.Controls.Add(this.textBoxInText);
             this.Controls.Add(this.buttonNext);
-            this.Controls.Add(this.button2);
+            this.Controls.Add(this.buttonStartPause);
             this.Controls.Add(this.buttonPrev);
-            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.buttonStart);
-            this.Controls.Add(this.comboBoxGraph);
-            this.Controls.Add(this.comboBoxSearch);
-            this.Controls.Add(this.comboBoxSort);
             this.Name = "FormSort";
-            this.Text = "Visualization";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.Text = "Sorting algorithms";
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-        private System.Windows.Forms.ComboBox comboBoxSort;
-        private System.Windows.Forms.ComboBox comboBoxSearch;
-        private System.Windows.Forms.ComboBox comboBoxGraph;
         private System.Windows.Forms.Button buttonStart;
-        private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Button buttonPrev;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button buttonStartPause;
         private System.Windows.Forms.Button buttonNext;
         private System.Windows.Forms.TextBox textBoxInText;
         private System.Windows.Forms.TextBox textBoxOutText;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.RichTextBox richTextBox;
+        private System.Windows.Forms.Button buttonBubble;
+        private System.Windows.Forms.Button buttonQuick;
+        private System.Windows.Forms.Button buttonInsert;
+        private System.Windows.Forms.Button buttonSelection;
+        private System.Windows.Forms.Button buttonHeapsort;
+        private System.Windows.Forms.Button buttonShell;
+        private System.Windows.Forms.Button buttonTimsort;
+        private System.Windows.Forms.Panel panel1;
     }
 }
 
--- Visualisation/Visualisation/FormSort.cs
+++ Visualisation/Visualisation/FormSort.cs
@@ -6,15 +6,161 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Media;
 using System.Windows.Forms;
 
 namespace Visualisation
 {
     public partial class FormSort : Form
     {
+        private int[] numbers;
+        private const int MAX_WIDTH = 593;
+        private int MAX_HEIGHT = 445;
+        private Rectangle[] rectangles;
+        Graphics graphics;
+
         public FormSort()
         {
             InitializeComponent();
+        }
+
+        // Пузырьковая сортировка
+        private void buttonBubble_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        // Быстрая сортировка
+        private void buttonQuick_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        // Сортировка выбором
+        private void buttonSelection_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        // Пирамидальная сортировка
+        private void buttonHeapsort_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        //Сортировка вставками
+        private void buttonInsert_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        // Сортировка Шелла
+        private void buttonShell_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        // TimSort
+        private void buttonTimsort_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            textBoxOutText.Text = "";
+            numbers = GetNumbersFromTextBox();
+            InitializeRectangles(numbers);
+        }
+
+        private int[] GetNumbersFromTextBox()
+        {
+            string inputNum = textBoxInText.Text;
+
+            if (inputNum.Equals(""))
+            {
+                //Random random = new Random();
+                //int randomNumber = random.Next(0, 100);
+                inputNum = "6 4 7 2 8 7 9 5 6 8 1";
+                textBoxInText.Text = inputNum;
+            }
+            
+            string[] arrayNum = inputNum.Split(' ');
+            int[] numbersLength = new int[arrayNum.Length];
+            for (int i = 0; i < arrayNum.Length; i++)
+            {
+                if (!arrayNum[i].Equals(""))
+                {
+                    numbersLength[i] = Int32.Parse(arrayNum[i]);
+                }
+            }
+            return numbersLength;
+        }
+
+        private void InitializeRectangles(int[] array)
+        {
+            if (HasNegative(array))
+                MAX_HEIGHT = MAX_HEIGHT / 2;
+
+            int space = MAX_WIDTH / array.Length;
+            int maxNumber = GetMax(array);
+            int pixelsPerNumber = MAX_HEIGHT / maxNumber;
+
+            panel1.Controls.Clear();
+            rectangles = new Rectangle[array.Length];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                int width = 25;
+                int height = 0;
+                int top = 0;
+                if (array[i] < 0)
+                {
+                    height = (-1) * array[i] * pixelsPerNumber;
+                    top = MAX_HEIGHT;
+                }
+                else
+                {
+                    height = array[i] * pixelsPerNumber;
+                    top = MAX_HEIGHT - height;
+                }
+                int left = space * i;
+                Rectangle rectangle = new Rectangle();
+                string arr = "12";
+                Font font1 = new Font("Arial", 12, FontStyle.Bold, GraphicsUnit.Point);
+                TextFormatFlags flags = TextFormatFlags.WordBreak;
+                graphics = panel1.CreateGraphics();
+                SolidBrush myBrush = new SolidBrush(Color.Red);
+                Pen pen = new Pen(Color.Black);
+                graphics.DrawRectangle(pen, left, top, width, height);
+                graphics.DrawString(arr, font1, Brushes.Blue, rectangle);
+                //graphics.FillRectangle(myBrush, left, top, width, height);
+                rectangles[i] = rectangle;
+            }
+        }
+
+        private int GetMax(int[] array)
+        {
+            int temp = array[0];
+            foreach (int n in array)
+            {
+                if (n < 0)
+                {
+                    if (-1 * n > temp) temp = -1 * n;
+                }
+                if (n > temp) temp = n;
+            }
+            return temp;
+        }
+
+        private bool HasNegative(int[] array)
+        {
+            foreach (int n in array)
+            {
+                if (n < 0) return true;
+            }
+            return false;
         }
     }
 }
